28419150969642a8980882031e6f5778a65d4aba
rinfromniigata <eternalfriendship70@gmail.com>
2026-02-12 16:00:09 +0900

    footer localization

---
+++ astroprojects/src/data/ui.ts
@@ -0,0 +1,26 @@
+export const ui = {
+    jp: {
+        footer: {
+            sitemap: "サイトマップ",
+            links: "リンク",
+            description: "技術と創造性、そして革新。",
+            rights: "All rights reserved.",
+        },
+    },
+    en: {
+        footer: {
+            sitemap: "Sitemap",
+            links: "Links",
+            description: "Technology, Creativity, and Innovation.",
+            rights: "All rights reserved.",
+        },
+    },
+    zh: {
+        footer: {
+            sitemap: "站点地图",
+            links: "链接",
+            description: "技术、创意与创新。",
+            rights: "版权所有。",
+        },
+    },
+} as const;
