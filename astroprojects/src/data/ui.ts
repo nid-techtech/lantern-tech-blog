b38d495861027e56dbfac45f3aeb71e013bbd8ab
rinfromniigata <eternalfriendship70@gmail.com>
2026-02-12 17:13:41 +0900

    footer fix

--- astroprojects/src/data/ui.ts
+++ astroprojects/src/data/ui.ts
@@ -2,24 +2,27 @@ export const ui = {
     jp: {
         footer: {
             sitemap: "サイトマップ",
-            links: "リンク",
-            description: "技術と創造性、そして革新。",
+            links: "外部リンク",
+            description: "ほおずきは真実を覆い隠す。我々は見えない真実を表現する。",
+            poweredby: "Lantern-TECHは 長岡造形大学所属 TECH☆TECH 先端メディア表現サークルの一員です。",
             rights: "All rights reserved.",
         },
     },
     en: {
         footer: {
             sitemap: "Sitemap",
-            links: "Links",
-            description: "Technology, Creativity, and Innovation.",
+            links: "External Links",
+            description: "The lantern plants vail the truth. We express the unseen truth.",
+            poweredby: "Lantern-TECH is powered by TECH☆TECH Advanced Media Design Society, affiliated with Nagaoka Institute of Design.",
             rights: "All rights reserved.",
         },
     },
     zh: {
         footer: {
             sitemap: "站点地图",
-            links: "链接",
-            description: "技术、创意与创新。",
+            links: "外部链接",
+            description: "酸浆果掩盖真相，我们表现看不见的真相。",
+            poweredby: "Lantern-TECH 是长冈造形大学所属的TECH☆TECH尖端媒体表现社团的一员。",
             rights: "版权所有。",
         },
     },
